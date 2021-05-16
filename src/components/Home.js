dc7159b337988433b59c3fc74d18792f2563c621
yoseph ahmed <yosephahmed@aol.com>
2021-05-16 12:25:38 -0400

    feat: implemented home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello from Home</h1>
+    <div className="jumbotron jumbotron-fluid vh-100">
+      <div className="container">
+        <div className="embed-responsive embed-responsive-16by9">
+          <video src="/videos/video.mp4" autoPlay loop muted />
+        </div>
+        <div className="d-flex flex-row justify-content-between mt-3">
+          <div>
+            <h1 className="display-4">Console Haven</h1>
+            <p className="h1 lead">The Ultimate Console Database For Gamers!</p>
+          </div>
+          <div className="align-self-center">
+            <Link to="/consoles">
+              <button type="button" className="btn btn-primary btn-lg">
+                View All
+              </button>
+            </Link>
+            <Link to="/lookup">
+              <button type="button" className="btn btn-danger btn-lg">
+                Search
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,30 +4,48 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-      <Link to="/" className="navbar-brand">
+      <Link to="/" className="navbar-brand text-white text-uppercase">
         Console Haven
-        <i class="fas fa-gamepad"></i>
+        <i class="fas fa-gamepad pl-1"></i>
       </Link>
-
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span>
+          <i className="fas fa-bars text-white"></i>
+        </span>
+      </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
-            <Link className="nav-link text-white text-uppercase ml-5" to="#">
+            <Link className="nav-link text-white text-uppercase ml-5" to="/">
               Home
-              <i class="fas fa-home"></i>
+              <i class="fas fa-home pl-1"></i>
               <span className="sr-only">(current)</span>
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white text-uppercase ml-5" to="#">
+            <Link
+              className="nav-link text-white text-uppercase ml-5"
+              to="/consoles"
+            >
               All Consoles
-              <i class="fas fa-layer-group"></i>
+              <i class="fas fa-layer-group pl-1"></i>
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white text-uppercase ml-5" to="#">
+            <Link
+              className="nav-link text-white text-uppercase ml-5"
+              to="/lookup"
+            >
               Console Lookup
-              <i class="fas fa-search"></i>
+              <i class="fas fa-search pl-1"></i>
             </Link>
           </li>
         </ul>
